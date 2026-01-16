4baf45853ba99e1263ba5f245d95842ff9ea4d9d
WOLFPAIR <wolfpair@mail.ru>
2026-01-17 02:50:03 +0500

    добавил иконку ютуба и хедер

--- frontend/src/components/header/button/button.tsx
+++ frontend/src/components/header/button/button.tsx
@@ -1,9 +1,7 @@
 import './button.css';
-
+import side_bar_icon from '../../../assets/sidebar-left-svgrepo-com.svg'
 export function Button() {
   return (
-    <button>
-      <span>Click me</span>
-    </button>
+    <button className='button_for_h'><img src={side_bar_icon} className='for_img_but'/></button>
   );
 }
--- frontend/src/pages/dashboard/dashboard.tsx
+++ frontend/src/pages/dashboard/dashboard.tsx
@@ -4,13 +4,13 @@ import { YoutubeIcon } from '../../components/youtube_icon/youtube_icon.tsx';
 // C:\Users\wolfp\OneDrive\Desktop\project_hex\project_hex\frontend\src\components\youtube_icon\youtube_icon.tsx
 export default function Dashboard() {
   return (
-    <header className='header'>
-      <h1>saite</h1> 
-   <div>
-    <button><p>кнопка</p></button>
-
-   </div>
-       <YoutubeIcon></YoutubeIcon>
+  <header className='header'>
+        <div>
+      <Button>Click</Button>
+    </div> 
+    <div>
+      <YoutubeIcon></YoutubeIcon>
+     </div>
    </header>
   );
 }
